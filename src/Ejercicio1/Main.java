f65b204f7e3159605a64f1bea8b66232f74487ae
EmanuelBenitezBaez <emanuelbenitezbaez@gmail.com>
2022-12-21 13:03:09 -0300

    Initial commit

---
+++ src/Ejercicio1/Main.java
@@ -0,0 +1,52 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args)
+    {
+// Tipos de datos.
+
+//Enteros:
+        System.out.println("\nENTEROS: \n");
+        byte variable1=10;
+        System.out.println("Byte:"+ variable1);
+
+        short variable2=2;
+        System.out.println("Short:"+ variable2);
+
+        int variable3=4;
+        System.out.println("Int:"+ variable3);
+        long variable4=6789410;
+        System.out.println("Long:"+ variable4);
+
+
+
+
+//Decimales:
+        System.out.println("\nDECIMALES:\n");
+        float variable5=54.54f;
+        System.out.println("Float:"+ variable5);
+
+        double variable6=65.5d;
+        System.out.println("Double:"+ variable6);
+
+
+//Boleanos:
+        System.out.println("\nBOOLEANOS:\n") ;
+        boolean variable7=true;
+        System.out.println("Boolean:"+ variable7);
+
+        boolean variable8=false;
+        System.out.println("Boolean:"+ variable8);
+
+
+//Texto:
+        System.out.println("\nTEXTOS:\n");
+        String variable9="Aqui va texto..";
+        System.out.println("String:"+ variable9);
+
+        char variable10='c';
+        System.out.println("Char:" + variable10);
+
+
+    }
+    }
